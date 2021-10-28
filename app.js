d34e67c03f8f3331807683a30ab6f99fb5ca4a1a
JannGarcia <jann.garcia@upr.edu>
2021-10-28 12:58:21 -0400

    Added session functionality

--- app.js
+++ app.js
@@ -1,49 +1,72 @@
 const express = require('express');
+const session = require('express-session');
 const http = require('http');
 const bcrypt = require('bcrypt');
 const path = require("path");
 const bodyParser = require('body-parser');
 const users = require('./data').userDB;
+const e = require('express');
 
 const app = express();
 const server = http.createServer(app);
 
+// Global Session: not reccommended and TEMPORARY
+var sess;
+
+app.use(session({
+  
+    // It holds the secret key for session
+    secret: 'Your_Secret_Key',
+  
+    // Forces the session to be saved
+    // back to the session store
+    resave: true,
+  
+    // Forces a session that is "uninitialized"
+    // to be saved to the store
+    saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'./HIP Website')));
 
-
-app.get('/',(req,res) => {
+app.get('/', function(req,res) {
+    sess = req.session;
     res.sendFile(path.join(__dirname,'./HIP Website/index.html'));
 });
 
-
 app.post('/register', async (req, res) => {
-    try{
-        let foundUser = users.find((data) => req.body.email === data.email);
-        if (!foundUser) {
-    
-            let hashPassword = await bcrypt.hash(req.body.password, 10);
-    
-            let newUser = {
-                id: Date.now(),
-                username: req.body.username,
-                email: req.body.email,
-                passwordEncrypted: hashPassword,
-                password: req.body.password
-            };
-            users.push(newUser);
-            console.log('User list', users);
-    
-            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./register.html'>Register another user</a></div>");
-        } else {
-            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./register.html'>Register again</a></div>");
+    sess = req.session;
+    if (sess.email){ // Check if the email is present
+        res.send(`<div align ='center'><h2>You are already logged in!</h2></div><br><br><br><div align ='center'><h3>Your email is: ${sess.email}</h3></div><br><br><div align='center'><a href=\'/logout'>click to logout</a></div>`);
+    }
+    else{
+        try{
+            let foundUser = users.find((data) => req.body.email === data.email);
+            if (!foundUser) {
+        
+                let hashPassword = await bcrypt.hash(req.body.password, 10);
+        
+                let newUser = {
+                    id: Date.now(),
+                    username: req.body.username,
+                    email: req.body.email,
+                    passwordEncrypted: hashPassword,
+                    password: req.body.password
+                };
+                users.push(newUser);
+                console.log('User list', users);
+                res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./register.html'>Register another user</a></div>");
+            } else {
+                res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./register.html'>Register again</a></div>");
+            }
+        } catch{
+            res.send("Internal server error");
         }
-    } catch{
-        res.send("Internal server error");
     }
 });
 
 app.post('/login', async (req, res) => {
+    sess = req.session;
     try{
         let foundUser = users.find((data) => req.body.username === data.username);
         if (foundUser) {
@@ -53,8 +76,9 @@ app.post('/login', async (req, res) => {
     
             const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
             if (passwordMatch) {
-                let usrname = foundUser.username;
-                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+                sess.username = foundUser.username;
+                sess.email = foundUser.email;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${sess.username}</h3></div><br><br><div align='center'><a href=\'/logout'>click to logout</a></div>`);
             } else {
                 res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
             }
@@ -69,6 +93,23 @@ app.post('/login', async (req, res) => {
     } catch{
         res.send("Internal server error");
     }
+});
+
+app.get('/logout', async(req, res) =>{
+    sess = req.session;
+
+    // For testing. 
+    if (sess.username){
+        console.log("Goodbye " + req.session.username);
+    }
+
+    else{
+        console.log("User isn't logged in!");
+    }
+
+    // Destroy the session, and redirect to the main page
+    sess.destroy();
+    return res.redirect('/');
 });
 
 server.listen(3000, function(){
