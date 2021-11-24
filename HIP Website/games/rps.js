e4d022e6d11e082a6f083fa09243ed0f5d3b2022
JannGarcia <jann.garcia@upr.edu>
2021-11-24 10:54:34 -0400

    WIP: Extracting Lobby

--- HIP Website/games/rps.js
+++ HIP Website/games/rps.js
@@ -1,7 +1,7 @@
-const socket = io("http://localhost:3000", { autoConnect: false });
-
+const socket = io('http://localhost:3000', { autoConnect: true });
 const canvas = document.getElementById('canvasrps');
 const ctx = canvas.getContext('2d');
+const rpsOptions = { ROCK: 'r', PAPER: 'p', SCISSORS: 's' };
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
@@ -16,14 +16,14 @@ const readyBtn = document.getElementById('ready');
 
 //images
 var rockImg = new Image();
-rockImg.src = "../images/rock1.png";
+rockImg.src = '../images/rock1.png';
 var paperImg = new Image();
-paperImg.src = "../images/paper1.png";
+paperImg.src = '../images/paper1.png';
 var scissorImg = new Image();
-scissorImg.src = "../images/scissor1.png";
+scissorImg.src = '../images/scissor1.png';
 
 //opponent
-const opponentPickElement = document.getElementById("opponentsPick");
+const opponentPickElement = document.getElementById('opponentsPick');
 
 //scores
 const playersScore = document.getElementById('pScore');
@@ -42,93 +42,124 @@ const pNameEl = document.getElementById('pNameEl');
 const oNameEl = document.getElementById('oNameEl');
 
 //player class
-class Player{
-    constructor(name, x,y,width,height){
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.currentSelected = 'r';
-        this.ready = false;
-    }
+class RPSPlayer {
+	constructor(name, x, y, width, height) {
+		this.username = name;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.currentSelected = rpsOptions.ROCK;
+		this.ready = false;
+	}
 
-    update(){
-        this.draw();
-        //this resets the x,y,width, and height properties of the player every update 
-        //it makes sure that if the screen is resized it keeps the same ratio and not the original values
-        this.x = canvas.width/8;
-        this.y = canvas.height/4;
-        this.width = canvas.width/14;
-        this.height = canvas.width/14;
-    }
+	update() {
+		this.draw();
+		//this resets the x,y,width, and height properties of the player every update
+		//it makes sure that if the screen is resized it keeps the same ratio and not the original values
+		this.x = canvas.width / 8;
+		this.y = canvas.height / 4;
+		this.width = canvas.width / 14;
+		this.height = canvas.width / 14;
+	}
 
-    draw(){
-        ctx.beginPath();
+	draw() {
+		ctx.beginPath();
 
-        if(this.currentSelected=='r'){
-            ctx.drawImage(rockImg,this.x,this.y,this.width*2,this.height*2);
-        }
-        else if(this.currentSelected=='p'){
-            ctx.drawImage(paperImg,this.x,this.y,this.width*2,this.height*2);
-        }        
-        else{//or 's'
-            ctx.drawImage(scissorImg,this.x,this.y,this.width*2,this.height*2);
-        }
+		if (this.currentSelected == rpsOptions.ROCK) {
+			ctx.drawImage(
+				rockImg,
+				this.x,
+				this.y,
+				this.width * 2,
+				this.height * 2
+			);
+		} else if (this.currentSelected == rpsOptions.PAPER) {
+			ctx.drawImage(
+				paperImg,
+				this.x,
+				this.y,
+				this.width * 2,
+				this.height * 2
+			);
+		} else {
+			//or rpsOptions.SCISSORS
+			ctx.drawImage(
+				scissorImg,
+				this.x,
+				this.y,
+				this.width * 2,
+				this.height * 2
+			);
+		}
 
-        ctx.fill();
-    }
+		ctx.fill();
+	}
 
-    changeSelection(newSelection){
-        this.currentSelected=newSelection;
-    }
+	changeSelection(newSelection) {
+		this.currentSelected = newSelection;
+	}
 }
 
+class Opponent {
+	constructor(name, x, y, width, height) {
+		this.username = name;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.currentSelected = rpsOptions.ROCK;
+		this.ready = false;
+	}
 
-class Opponent{
-    constructor(name, x,y,width,height,){
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.currentSelected = 'r';
-        this.ready = false;
-    }
+	update() {
+		//should search for a change of the choice in the databaase and if they player clicked ready
+		//this resets the x,y,width, and height properties of the opponent every update
+		//it makes sure that if the screen is resized it keeps the same ratio and not the original values
+		this.x = canvas.width - canvas.width / 4;
+		this.y = canvas.height / 4;
+		this.width = canvas.width / 14;
+		this.height = canvas.width / 14;
+	}
 
-    update(){
-        //should search for a change of the choice in the databaase and if they player clicked ready
-        //this resets the x,y,width, and height properties of the opponent every update 
-        //it makes sure that if the screen is resized it keeps the same ratio and not the original values
-        this.x = canvas.width - canvas.width/4;
-        this.y = canvas.height/4;
-        this.width = canvas.width/14;
-        this.height = canvas.width/14;
-    }
-    
-    draw(){
-       if(this.ready){
-        ctx.beginPath();
+	draw() {
+		if (this.ready) {
+			ctx.beginPath();
 
-        if(this.currentSelected=='r'){
-            ctx.drawImage(rockImg,this.x,this.y,this.width*2,this.height*2);
-        }
-        else if(this.currentSelected=='p'){
-            ctx.drawImage(paperImg,this.x,this.y,this.width*2,this.height*2);
-        }        
-        else{
-            ctx.drawImage(scissorImg,this.x,this.y,this.width*2,this.height*2);
-        }
+			if (this.currentSelected == rpsOptions.ROCK) {
+				ctx.drawImage(
+					rockImg,
+					this.x,
+					this.y,
+					this.width * 2,
+					this.height * 2
+				);
+			} else if (this.currentSelected == rpsOptions.PAPER) {
+				ctx.drawImage(
+					paperImg,
+					this.x,
+					this.y,
+					this.width * 2,
+					this.height * 2
+				);
+			} else {
+				ctx.drawImage(
+					scissorImg,
+					this.x,
+					this.y,
+					this.width * 2,
+					this.height * 2
+				);
+			}
 
-        ctx.fill();
-       } 
-    }
+			ctx.fill();
+		}
+	}
 
-    changeSelection(newSelection){
-        this.currentSelected=newSelection;
-    }
+	changeSelection(newSelection) {
+		this.currentSelected = newSelection;
+	}
 }
-
 
 let pScore = 0;
 let oScore = 0;
@@ -139,198 +170,226 @@ let animationId;
 let countDownTimerId;
 
 //the names are set later once players join*
-let player = new Player("", canvas.width/8,canvas.height/4,canvas.width/14,canvas.width/14);
-let opponent = new Opponent("", canvas.width-canvas.width/4, canvas.height/4,canvas.width/14,canvas.width/14);
+let player = new RPSPlayer(
+	'',
+	canvas.width / 8,
+	canvas.height / 4,
+	canvas.width / 14,
+	canvas.width / 14
+);
+let opponent = new Opponent(
+	'',
+	canvas.width - canvas.width / 4,
+	canvas.height / 4,
+	canvas.width / 14,
+	canvas.width / 14
+);
 
-let lobbyID;
+let currentLobby;
 
+function animate() {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
 
-function animate(){
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;   
-    
-    animationId = requestAnimationFrame(animate);
-    ctx.fillStyle = 'rgb(26, 24, 24)';//sets the color to the background color
-    ctx.fillRect(0,0,canvas.width,canvas.height);//fills the background after every draw
+	animationId = requestAnimationFrame(animate);
+	ctx.fillStyle = 'rgb(26, 24, 24)'; //sets the color to the background color
+	ctx.fillRect(0, 0, canvas.width, canvas.height); //fills the background after every draw
 
-    
-    player.update();
-    opponent.update();
-    if(roundOver){
-        //if the round is over, first verify if the game is over before starting a new round
-        if(pScore == 3){
-            nextRoundEl.innerHTML = "You won!! Redirecting to play page in " + nextRoundCountDown + "...";
-            if(nextRoundCountDown <= 0){
-                //move to the play page
-                window.location = '/play';
-            }
-        }
-        else if(oScore == 3){
-            nextRoundEl.innerHTML = "You Lost!! Redirecting to play page in " + nextRoundCountDown + "...";
-            if(nextRoundCountDown <= 0){
-                //move to the play page
-                window.location = '/play';
-            }
-        }
-        else{
-            nextRoundEl.innerHTML = "Next round in " + nextRoundCountDown + "...";
-            if(nextRoundCountDown <= 0){
-                resetRound();
-            }
-        } 
-        opponent.draw();
-    }
+	player.update();
+	opponent.update();
+	if (roundOver) {
+		//if the round is over, first verify if the game is over before starting a new round
+		if (pScore == 3) {
+			nextRoundEl.innerHTML =
+				'You won!! Redirecting to play page in ' +
+				nextRoundCountDown +
+				'...';
+			if (nextRoundCountDown <= 0) {
+				//move to the play page
+				window.location = '/play';
+			}
+		} else if (oScore == 3) {
+			nextRoundEl.innerHTML =
+				'You Lost!! Redirecting to play page in ' +
+				nextRoundCountDown +
+				'...';
+			if (nextRoundCountDown <= 0) {
+				//move to the play page
+				window.location = '/play';
+			}
+		} else {
+			nextRoundEl.innerHTML =
+				'Next round in ' + nextRoundCountDown + '...';
+			if (nextRoundCountDown <= 0) {
+				resetRound();
+			}
+		}
+		opponent.draw();
+	}
 
-    if(opponent.name == ""){
-        opponentPickElement.innerHTML = "Waiting on opponent to join...";
-    }
-    else if(!opponent.ready){
-        opponentPickElement.innerHTML = "Picking...";
-    }
-
-    else if(!player.ready && opponent.ready){
-        opponentPickElement.innerHTML = "Picked!";
-    }   
-    
-    else if((player.ready && opponent.ready) && !roundOver){
-        opponentPickElement.innerHTML = null;
-        pickRoundkWinnerAndUdpateScore();
-        roundOverCountDown();//calls the countdown only once since it has an interval in the inside!
-        roundOver=true;
-    }
+	if (opponent.username == '') {
+		opponentPickElement.innerHTML = 'Waiting on opponent to join...';
+	} else if (!opponent.ready) {
+		opponentPickElement.innerHTML = 'Picking...';
+	} else if (!player.ready && opponent.ready) {
+		opponentPickElement.innerHTML = 'Picked!';
+	} else if (player.ready && opponent.ready && !roundOver) {
+		opponentPickElement.innerHTML = null;
+		pickRoundkWinnerAndUdpateScore();
+		roundOverCountDown(); //calls the countdown only once since it has an interval in the inside!
+		roundOver = true;
+	}
 }
 
-
-function pickRoundkWinnerAndUdpateScore(){
-    if(player.currentSelected == opponent.currentSelected){
-        //tie
-        roundRes1.innerHTML = "tie!";
-        roundRes2.innerHTML = "tie!";
-    }
-    else if((player.currentSelected == 'p' && opponent.currentSelected == 'r') 
-        || (player.currentSelected == 'r' && opponent.currentSelected == 's') 
-        || (player.currentSelected == 's' && opponent.currentSelected == 'p')){
-        //player wins
-        playersScore.innerHTML = ++pScore;
-        roundRes1.innerHTML = "Round Winner!";
-        roundRes2.innerHTML = null;
-    }
-    else{
-        //if player didn't win, and it wasn't a tie, then opponent won
-        opponentsScore.innerHTML = ++oScore;
-        roundRes1.innerHTML = null;
-        roundRes2.innerHTML = "Round Winner!";
-    }
+function pickRoundkWinnerAndUdpateScore() {
+	if (player.currentSelected == opponent.currentSelected) {
+		//tie
+		roundRes1.innerHTML = 'tie!';
+		roundRes2.innerHTML = 'tie!';
+	} else if (
+		(player.currentSelected == rpsOptions.PAPER &&
+			opponent.currentSelected == rpsOptions.ROCK) ||
+		(player.currentSelected == rpsOptions.ROCK &&
+			opponent.currentSelected == rpsOptions.SCISSORS) ||
+		(player.currentSelected == rpsOptions.SCISSORS &&
+			opponent.currentSelected == rpsOptions.PAPER)
+	) {
+		//player wins
+		playersScore.innerHTML = ++pScore;
+		roundRes1.innerHTML = 'Round Winner!';
+		roundRes2.innerHTML = null;
+	} else {
+		//if player didn't win, and it wasn't a tie, then opponent won
+		opponentsScore.innerHTML = ++oScore;
+		roundRes1.innerHTML = null;
+		roundRes2.innerHTML = 'Round Winner!';
+	}
 }
 
-
-function roundOverCountDown(){
-    countDownTimerId = setInterval(() => {
-        nextRoundCountDown--;
-    },1000);//every second it ticks down the counter (1000 ms)
+function roundOverCountDown() {
+	countDownTimerId = setInterval(() => {
+		nextRoundCountDown--;
+	}, 1000); //every second it ticks down the counter (1000 ms)
 }
 
+async function resetRound() {
+	player.changeSelection(rpsOptions.ROCK);
+	player.ready = false;
 
-async function resetRound(){
-    player.changeSelection('r');
-    player.ready = false; 
-    
-    opponent.changeSelection('r');
-    opponent.ready = false;
+	opponent.changeSelection(rpsOptions.ROCK);
+	opponent.ready = false;
 
-    //refer to the side note in inside the event listener for the ready btn 
-    readyBtn.style.display="initial";
-    rockBtn.style.display="initial";
-    paperBtn.style.display="initial";
-    scissorBtn.style.display="initial";
-    
-    roundRes1.innerHTML = null;
-    roundRes2.innerHTML = null;
+	//refer to the side note in inside the event listener for the ready btn
+	readyBtn.style.display = 'initial';
+	rockBtn.style.display = 'initial';
+	paperBtn.style.display = 'initial';
+	scissorBtn.style.display = 'initial';
 
-    nextRoundEl.innerHTML = null;
+	roundRes1.innerHTML = null;
+	roundRes2.innerHTML = null;
 
-    clearInterval(countDownTimerId);//cancel the counter such that it doesn't keep running 
-    nextRoundCountDown = 5;//this line and the one above are not interchangeable**
+	nextRoundEl.innerHTML = null;
 
-    //new round, check if opponent shot again (starts the loop on "app.js")
-    await socket.emit("getOpponentShot", lobbyID, player.name, opponent.name);
-    roundOver = false;
-   
+	clearInterval(countDownTimerId); //cancel the counter such that it doesn't keep running
+	nextRoundCountDown = 5; //this line and the one above are not interchangeable**
+
+	startGame();
+	roundOver = false;
 }
-
 
 //listeners for the buttons
 rockBtn.addEventListener('click', () => {
-    player.changeSelection('r');
+	player.changeSelection(rpsOptions.ROCK);
 });
 
 paperBtn.addEventListener('click', () => {
-    player.changeSelection('p');
+	player.changeSelection(rpsOptions.PAPER);
 });
 
 scissorBtn.addEventListener('click', () => {
-    player.changeSelection('s');
+	player.changeSelection(rpsOptions.SCISSORS);
 });
 
 readyBtn.addEventListener('click', () => {
-    socket.emit("action", player.name + " shot " + player.currentSelected);
-    player.ready = true;
+	socket.emit('action', `${player.username} ' shot:' ${player.currentSelected}`);
+	player.ready = true;
 
-    //removes all the buttons such that they can't be used if the player is ready
-    //side note, there are several aways to accomplish this, though i picked this one as it works in the case of the game and the purpose of "removing them"
-    readyBtn.style.display="none";
-    rockBtn.style.display="none";
-    paperBtn.style.display="none";
-    scissorBtn.style.display="none";
+	//removes all the buttons such that they can't be used if the player is ready
+	//side note, there are several aways to accomplish this, though i picked this one as it works in the case of the game and the purpose of "removing them"
+	readyBtn.style.display = 'none';
+	rockBtn.style.display = 'none';
+	paperBtn.style.display = 'none';
+	scissorBtn.style.display = 'none';
 });
 
+function startGame(lobby) {
+	let action = `${opponent.username} shot:`;
 
-//socket listeners
-socket.connect();
+	// Wait for the opponent to shoot, then remove their action
+	socket.emit('waitForAction', action);
+	opponent.ready = true;
+	opponent.currentSelected = currentSelected;
+	socket.emit('removeAction', action);
+}
 
-socket.on("connect", async () => {
-    socket.emit("join", Date.now(), "RPS");
-
+// When the user connects, join an available lobby!
+// Emits either 'noLobbyFound', or 'lobbyFound'
+socket.on('connect', async () => {
+    console.log("Someone connected!");
+	socket.emit('findAvailableLobby', 'RPS');
 });
 
-
-socket.on("joined", async (lobbyId, player1, player2) =>{
-    if(player2 == ""){//then "this" joined as the player1
-        player.name = player1;
-        pNameEl.innerHTML = player1;
-        //start a loop in app.js that looks for when player2 joins
-        await socket.emit('checkPlayer2', (lobbyId));
-        //opponent's name stays empty
-
-    }else{//then "this" joined as the player2
-        player.name = player2;
-        opponent.name = player1;
-        pNameEl.innerHTML = player2;
-        oNameEl.innerHTML = player1;
-        //call the actions since both players are now inside the section
-        await socket.emit("getOpponentShot", lobbyId, player.name, opponent.name);
-        //if this doesn't make sense or seems confusing please feel free to ask^
-    }
-
-    lobbyID = lobbyId;
-    console.log(lobbyID);
-
-    socket.emit('action', player.name +  " joined the lobby.");
-    animate();
+// Create a lobby if none is found.
+// Emits 'createLobbySuccess' on creation
+socket.on('noLobbyFound', async () => {
+    console.log("No lobby found. Creating new lobby");
+	socket.emit('createLobby', Date.now(), 'RPS', 2);
 });
 
-
-socket.on("setPlayer2", async (player2) =>{//gets back the opponent if he joined
-    console.log(player2 + "joined");
-    opponent.name = player2;
-    oNameEl.innerHTML = player2;
-    //call the actions since both players are now inside the section
-    await socket.emit("getOpponentShot", lobbyID, player.name, opponent.name);
-    
+// A vacant lobby was found. Join it!
+// Emits 'joinedSuccessfully' or 'failedToJoin'
+socket.on('lobbyFound', async (lobby) => {
+    console.log("Found lobby: " + lobby);
+	socket.emit('joinLobby', lobby._id, player);
 });
 
+// A lobby was creatted successfully, join it!
+// Emits 'joinedSuccessfully' or 'failedToJoin'
+socket.on('createLobbySuccess', async (lobby) => {
+    console.log("Lobby created successfully!");
+	socket.emit('joinLobby', lobby._id, player);
+});
 
-socket.on("opponentShot", (opChoice) =>{
-    opponent.changeSelection(opChoice);
-    opponent.ready = true;
+socket.on('failedToJoin', async (lobby) => {});
+
+socket.on('joinedSuccessfully', async (lobby) => {
+	const player1 = lobby.players[0];
+	const player2 = lobby.players[1];
+	if (!player2) {
+		//then "this" joined as the player1
+		player.username = player1.username;
+		pNameEl.innerHTML = player1.username;
+		//start a loop in app.js that looks for when player2 joins
+		await socket.emit('waitUntilFull', lobby);
+	} else {
+		//then "this" joined as the player2
+		player.username = player2.username;
+		opponent.username = player1.username;
+		pNameEl.innerHTML = player2.username;
+		oNameEl.innerHTML = player1.username;
+
+		// Start the game!
+		startGame(lobby);
+	}
+
+	currentLobby = lobby;
+	console.log(lobby);
+
+	socket.emit('addAction', player.username + ' joined the lobby.');
+	animate();
+});
+
+socket.on('actionFound', (action) => {
+	opponent.changeSelection(action.substring(action.lastIndexOf(':') + 1));
+	opponent.ready = true;
 });
